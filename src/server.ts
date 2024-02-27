03358b2aebf1b1773c4b9077e11eb3c938a1f3c7
Juan Camilo Quintero Jimenez <110494864+Bryctan@users.noreply.github.com>
2024-02-27 12:42:46 -0500

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -37,7 +37,7 @@ class Server {
 
     middlewares() {
         this.app.use(express.json());
-        const allowedOrigins = ['https://proveerten.netlify.app','https://proveer-ten-front-kb5j9qyxj-bryctans-projects.vercel.app','http://localhost:4200'];
+        const allowedOrigins = ['https://proveerten.netlify.app','https://proveer-ten-front-end-git-hu-featurechat-bryctans-projects.vercel.app','http://localhost:4200'];
         this.app.use(cors({ credentials: true, origin: allowedOrigins }));
         this.app.use(morgan('dev'));
     }
